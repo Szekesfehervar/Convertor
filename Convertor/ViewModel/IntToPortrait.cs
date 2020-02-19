71bc516b6b279cc1ed040d5164cc80de695e02fc
Pepa <josef.simunek@pslib.cz>
2020-02-19 09:22:17 +0100

    Add project files.

---
+++ Convertor/ViewModel/IntToPortrait.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Converters.ViewModels.Converters
+{
+    class IntToPortrait : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int)
+            {
+                int cislo = ((int)value) % 7;
+                switch (cislo)
+                {
+                    case 0: { return Portrait1; }
+                    case 1: { return Portrait2; }
+                    case 2: { return Portrait3; }
+                    case 3: { return Portrait4; }
+                    case 4: { return Portrait5; }
+                    case 5: { return Portrait6; }
+                    case 6: { return Portrait7; }
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BitmapImage Portrait1 { get; set; }
+        public BitmapImage Portrait2 { get; set; }
+        public BitmapImage Portrait3 { get; set; }
+        public BitmapImage Portrait4 { get; set; }
+        public BitmapImage Portrait5 { get; set; }
+        public BitmapImage Portrait6 { get; set; }
+        public BitmapImage Portrait7 { get; set; }
+    }
+}
---
+++ Convertor/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertor.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        Random r = new Random();
+        public int _image
+        {
+            get
+            {
+                return _image;
+            }
+            set
+            {
+                _image = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            _image = 3;
+        }
+
+
+
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
